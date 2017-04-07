e95d1ede938b56f8137eaf984f4b159304e4a264
bali182 <edesbalazs@gmail.com>
2017-04-07 13:01:14 +0100

    removed defaultVisit added any instead. code more compact

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import { schemaType } from './types'
-import { noop, isFunction } from './utils'
+import { noop } from './utils'
 
-const createVisitor = (visitor = {}, defaultVisit = noop) => (schema, ...args) => {
-  const type = schemaType(schema)
-  const visitMethod = visitor[type]
-  const method = visitMethod && isFunction(visitMethod) ? visitMethod : defaultVisit
-  return method.apply(visitor, [schema, ...args])
-}
+const createVisitor = (visitor = {}) => 
+  (schema, ...args) => 
+  (visitor[schemaType(schema)] || visitor.any || noop).call(visitor, schema, ...args)
 
 export default createVisitor
--- src/utils.js
+++ src/utils.js
@@ -4,6 +4,4 @@ export const isObject = input => input instanceof Object && !isArray(input)
 
 export const isDefined = input => input !== null && typeof input !== 'undefined'
 
-export const isFunction = input => Boolean(input && input.constructor && input.call && input.apply)
-
 export const noop = () => { /* noop */ }
